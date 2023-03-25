94498bc71a560fd46b189953370ebfb0beebda1b
chpark-ML <bch0322@gmail.com>
2023-03-25 15:22:56 +0900

    add mnist dataset

--- projects/MNIST/main.py
+++ projects/MNIST/main.py
@@ -4,9 +4,15 @@ script example :
 `HYDRA_FULL_ERROR=1 python3 project/main.py`
 """
 import hydra
+import torch
 from omegaconf import DictConfig
 
 from project.train import train
+
+
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
 
 
 @hydra.main(version_base="1.3.2", config_path="configs", config_name="train.yaml")
--- projects/MNIST/utils/dataset.py
+++ projects/MNIST/utils/dataset.py
@@ -1,1 +1,14 @@
+from torch.utils.data import Dataset
+from torchvision.datasets import MNIST
+
 # dataset
+class MNIST_Dataset(Dataset):
+    def __init__(self, mode="train"):
+        self.x_data = MNIST(download_root, transform=mnist_transform, train=True, download=True)
+        self.y_data = None
+
+    def __getitem__(self, index): 
+        return self.x_data[index], self.y_data[index]
+        
+    def __len__(self): 
+        return self.x_data.shape[0]
