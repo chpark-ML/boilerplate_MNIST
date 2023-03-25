f9d54ebe8ac436b7b65c09f7f3cdb542c32b134c
chpark-ML <bch0322@gmail.com>
2023-03-25 15:54:52 +0900

    fix

--- projects/MNIST/train.py
+++ projects/MNIST/train.py
@@ -10,7 +10,28 @@ class trainer():
         params = filter(lambda p: p.requires_grad, self.model.parameters())
         self.optimier = hydra.utils.instantiate(cfg.optimizer, params)
         self.scheduler = hydra.utils.instantiate(cfg.scheduler)
-        
+    
+    def fit():
+        None
+
+
+def run_epoch():
+    pass
+
+
+def train_epoch():
+    pass
+
+
+@torch.no_grad()
+def val_epoch():
+    pass
+
+
+@torch.no_grad()
+def test_epoch():
+    pass
+
 
 def train(config: DictConfig):
     _trainer = trainer(config)
