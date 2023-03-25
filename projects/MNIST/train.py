b74679b4e86cd50dc14f9d79af285c1cef2c3c18
chpark-ML <bch0322@gmail.com>
2023-03-25 15:42:04 +0900

    add params in optimizer

--- projects/MNIST/train.py
+++ projects/MNIST/train.py
@@ -3,22 +3,12 @@ import numpy as np
 import torch
 from omegaconf import DictConfig
 
-seed = 1234
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-np.random.seed(seed)
-
-# Select GPUs
-use_cuda = not args.no_cuda and torch.cuda.is_available()
-gpu_list = [int(i) for i in args.gpu.strip().split(",")]
-device = torch.device(f"cuda:{gpu_list[0]}" if use_cuda else "cpu")
 
 class trainer():
     def __init__(self, cfg):
         self.model = hydra.utils.instantiate(cfg.model)
         params = filter(lambda p: p.requires_grad, self.model.parameters())
-        self.optimier = hydra.utils.instantiate(cfg.optimizer)
+        self.optimier = hydra.utils.instantiate(cfg.optimizer, params)
         self.scheduler = hydra.utils.instantiate(cfg.scheduler)
         
 
