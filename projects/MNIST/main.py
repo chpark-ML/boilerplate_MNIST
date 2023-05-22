d03808a00a2671a52356b07e0fce2773712c2dd2
Changhyun Park <41136459+chpark-ML@users.noreply.github.com>
2023-05-22 23:46:39 +0900

    Mnist dataset (#13)
    * add mnist dataset
    
    * fix
    
    * add params in optimizer
    
    * fix

--- projects/MNIST/main.py
+++ projects/MNIST/main.py
@@ -4,9 +4,11 @@ script example :
 `HYDRA_FULL_ERROR=1 python3 project/main.py`
 """
 import hydra
+import numpy as np
+import torch
 from omegaconf import DictConfig
 
-from project.train import train
+from projects.train import train
 
 
 @hydra.main(version_base="1.3.2", config_path="configs", config_name="train.yaml")
--- projects/MNIST/train.py
+++ projects/MNIST/train.py
@@ -1,4 +1,6 @@
 import hydra
+import numpy as np
+import torch
 from omegaconf import DictConfig
 
 
@@ -6,9 +8,30 @@ class trainer():
     def __init__(self, cfg):
         self.model = hydra.utils.instantiate(cfg.model)
         params = filter(lambda p: p.requires_grad, self.model.parameters())
-        # self.optimier = hydra.utils.instantiate(cfg.optimizer)
-        # self.scheduler = hydra.utils.instantiate(cfg.scheduler)
-        
+        self.optimier = hydra.utils.instantiate(cfg.optimizer, params)
+        self.scheduler = hydra.utils.instantiate(cfg.scheduler)
+    
+    def fit():
+        None
+
+
+def run_epoch():
+    pass
+
+
+def train_epoch():
+    pass
+
+
+@torch.no_grad()
+def val_epoch():
+    pass
+
+
+@torch.no_grad()
+def test_epoch():
+    pass
+
 
 def train(config: DictConfig):
     _trainer = trainer(config)
